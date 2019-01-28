3ddec29e209311921d158535067509faff6cf6ac
vincent2021 <vincent2021@gmail.com>
2019-01-28 23:25:14 +0100

    ex12 done

---
+++ d01/ex12/search_it!.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+$i = 2;
+while ($i < $argc) {
+  $tab[strstr($argv[$i], ":", -1)] = substr(strstr($argv[$i], ":"), 1);
+  $i++;
+}
+  if ($tab[$argv[1]]){
+  echo $tab[$argv[1]], "\n";
+}
+ ?>
