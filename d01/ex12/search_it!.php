6b773fd02c7a565c03a6777f5be67772eddac80b
vincent2021 <vincent2021@gmail.com>
2019-01-28 23:25:43 +0100

    ex12 done

--- d01/ex12/search_it!.php
+++ d01/ex12/search_it!.php
@@ -5,7 +5,7 @@ while ($i < $argc) {
   $tab[strstr($argv[$i], ":", -1)] = substr(strstr($argv[$i], ":"), 1);
   $i++;
 }
-  if ($tab[$argv[1]]){
-  echo $tab[$argv[1]], "\n";
+  if ($tab[$argv[1]]) {
+    echo $tab[$argv[1]], "\n";
 }
  ?>
