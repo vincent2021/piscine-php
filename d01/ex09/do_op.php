e0417487b0573312228273d23868c1fc2c347a64
vincent2021 <vincent2021@gmail.com>
2019-01-28 20:20:08 +0100

    do_op

---
+++ d01/ex09/do_op.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+if ($argc != 4) {
+  echo "Incorrect Parameters\n";
+} else {
+  $i = 1;
+  while ($i < $argc) {
+      $tab[] = trim($argv[$i]);
+      $i++;
+  }
+  print_r($tab);
+  if ($tab[1] == "+") {
+    echo $tab[0] + $tab[2],"\n";
+  }
+  if ($tab[1] == "-") {
+    echo $tab[0] - $tab[2], "\n";
+  }
+  if ($tab[1] == "/") {
+    echo $tab[0] / $tab[2], "\n";
+  }
+  //Gestion dans zsh hasardeuse
+  if ($tab[1] == "*") {
+    echo $tab[0] * $tab[2], "\n";
+  }
+  if ($tab[1] == "%") {
+    echo $tab[0] % $tab[2], "\n";
+  }
+}
+?>
