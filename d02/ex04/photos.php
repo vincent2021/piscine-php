a284ea28efa5285ec6d5168cec6b993bd8397086
Vincent MUCCHIELLI <vimucchi@e3r8p2.42.fr>
2019-02-01 17:28:29 +0100

    ex04

--- d02/ex04/photos.php
+++ d02/ex04/photos.php
@@ -5,7 +5,16 @@ if ($argv[1]) {
   $html = DOMDocument::loadHTMLFile($argv[1]);
   foreach ($html->getElementsByTagName("img") as $img) {
     $img_link = $img->getAttribute("src");
-    echo $img_link."\n";
+    $img_name = substr(strrchr($img_link, '/'), 1);
+    $folder_name = substr($argv[1], 7);
+    if (!is_dir($folder_name)) {
+      mkdir($folder_name);
+    }
+    if ($img_link[0] == '/') {
+      file_put_contents($folder_name.'/'.$img_name, file_get_contents($argv[1].$img_link));
+    } else {
+      file_put_contents($folder_name.'/'.$img_name, file_get_contents($img_link));
+    }
   }
 }
 ?>
