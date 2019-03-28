97c898f4a976b53dc2972d3803cbdf2e56c378d3
Vincent MUCCHIELLI <vimucchi@e3r8p9.42.fr>
2019-03-28 13:37:01 +0100

    update folder

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -1,7 +1,7 @@
 <?php
-header('HTTP/1.0 401 Unauthorized');
-header('WWW-Authenticate: Basic realm="42raw"');
 if ($_SERVER['PHP_AUTH_USER'] != "zaz" && $_SERVER['PHP_AUTH_PW'] != "jaimelespetitsponeys") {
+    header('HTTP/1.0 401 Unauthorized');
+    header('WWW-Authenticate: Basic realm="42raw"');
     echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>";
 } else {
     $raw = base64_encode(file_get_contents('../img/42.png'));
