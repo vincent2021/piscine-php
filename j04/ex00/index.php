31cbab0ad0bd616cb6bd6fab18e7037227d438af
Vincent MUCCHIELLI <vimucchi@e3r8p9.42.fr>
2019-03-29 00:41:50 +0100

    begin d04

--- j04/ex00/index.php
+++ j04/ex00/index.php
@@ -1,1 +1,19 @@
 <?php
+session_start();
+if ($_GET['submit'] == 'OK') {
+    $_SESSION['login'] = $_GET['login'];
+    $_SESSION['passwd'] = $_GET['passwd'];
+}
+$login = isset($_SESSION['login']) ? $_SESSION['login'] : '';
+$password = isset($_SESSION['passwd']) ? $_SESSION['passwd'] : '';
+?>
+<html><body>
+<form method="get" action="index.php">
+  <p>
+    Identifiant: <input type="text" name="login" value="<?= $login?>"/> <br>
+    Mot de passe: <input type="password" name="passwd" value="<?= $password?>"/><br>
+    <input type="submit" name="submit" value="OK">
+ </p>
+ </form>
+</body></html>
+ 
